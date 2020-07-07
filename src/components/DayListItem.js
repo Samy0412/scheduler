cf749664843b9d00a62017605ec17e5a40e86efe
Samy0412 <samantha.gadet@gmail.com>
2020-07-07 19:11:19 -0400

    day2- Listing Interviewers

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,14 +1,30 @@
 import React from "react";
-// var classnames = require("classnames");
+import "components/DayListItem.scss";
+var classnames = require("classnames");
 
 export default function DayListItem(props) {
-  // let dayItemClass = classnames({
-  //   selected: props.selected,
-  // });
+  let dayClass = classnames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
+  const formatSpots = () => {
+    switch (props.spots) {
+      case 0:
+        return "no spots";
+        break;
+      case 1:
+        return "1 spot";
+        break;
+      default:
+        return `${props.spots} spots`;
+    }
+  };
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={props.setDay}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()} remaining</h3>
     </li>
   );
 }
