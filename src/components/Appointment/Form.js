05128ae24e1c7e89dde2d47e1558df4949b34619
Samy0412 <samantha.gadet@gmail.com>
2020-07-07 20:37:24 -0400

    appointment form and displaying appointments

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,10 +5,22 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [name, setName] = useState(props.name || "");
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+  const save = () => {
+    props.onSave(name, interviewer);
+  };
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -26,10 +38,10 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={props.onSave}>
+          <Button confirm onClick={save}>
             Save
           </Button>
         </section>
