ca9a32366521c64f4c17789e3d4dee098a7905ed
Samy0412 <samantha.gadet@gmail.com>
2020-07-10 20:42:03 -0400

    handle errors

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,4 @@
-import React, { useState, useEffect } from "react";
-//Axios allows to make server resquests like $Ajax in Jquery
-import axios from "axios";
-
+import React from "react";
 //imports stylesheet
 import "components/Application.scss";
 //imports needed components
@@ -12,63 +9,16 @@ import {
   getInterview,
   getInterviewersForDay,
 } from "helpers/selectors";
+//Imports custom hook that manages the state
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  //Hook to store the state and update it
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {},
-  });
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    return axios
-      .put(`/api/appointments/${id}`, appointment)
-      .then(() => setState({ ...state, appointments }));
-  }
-
-  function deleteInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-    return axios
-      .delete(`/api/appointments/${id}`, appointment)
-      .then(() => setState({ ...state, appointments }));
-  }
-  //Function to update the state of the day
-  const setDay = (day) => setState({ ...state, day });
-  //Hook enclosing the API requests to be called after painting the DOM
-  useEffect(() => {
-    Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`),
-    ]).then((all) => {
-      //Updating the state of of the days, appointments and interviewers
-      setState((prev) => ({
-        //...prev" makes sure it it the current version og the state that is copied, not the version of the state at the time of the call
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data,
-      }));
-    });
-  }, []);
+  const {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview,
+  } = useApplicationData();
 
   //Displays all the interviewers for the selected days
   const interviewers = getInterviewersForDay(state, state.day);
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+//Axios allows to make server resquests like $Ajax in Jquery
+import axios from "axios";
+
+export default function useApplicationData() {
+  //Hook to store the state and update it
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
+  });
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios
+      .put(`/api/appointments/${id}`, appointment)
+      .then(() => setState({ ...state, appointments }));
+  }
+
+  function deleteInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    return axios
+      .delete(`/api/appointments/${id}`, appointment)
+      .then(() => setState({ ...state, appointments }));
+  }
+  //Function to update the state of the day
+  const setDay = (day) => setState({ ...state, day });
+  //Hook enclosing the API requests to be called after painting the DOM
+  useEffect(() => {
+    Promise.all([
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`),
+    ]).then((all) => {
+      //Updating the state of of the days, appointments and interviewers
+      setState((prev) => ({
+        //...prev" makes sure it it the current version og the state that is copied, not the version of the state at the time of the call
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
+  }, []);
+  return {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview,
+  };
+}
